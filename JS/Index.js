ea342d46d5ee2e18af26b6ccea8c598f98da5dfb
Martin <martin.cassan@gmail.com>
2025-08-13 17:27:42 +0200

    adding EN page

--- JS/Index.js
+++ JS/Index.js
@@ -1,8 +1,23 @@
-document.getElementById('button_1').addEventListener('click', function () {
+var left_button = document.getElementById('button_1')
+var right_button = document.getElementById('button_2')
+
+
+left_button.addEventListener('click', function () {
     this.style.display = 'none';
-    document.getElementById('button_2').style.display = 'inline-block';
+    right_button.textContent='Remettre' ;
 });
 
-document.getElementById('button_2').addEventListener('click', function () {
-    document.getElementById('button_1').style.display = 'inline-block';
+right_button.addEventListener('click', function () {
+    left_button.style.display = 'inline-block';
+    this.textContent='Rien';
 });
+
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(5, 5, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
